a8ea2e25642e3a0729822ff57f615c09141275ac
417-72KI <417.72ki@gmail.com>
2019-08-16 15:48:55 +0900

    [WIP]

--- TrapOfCollections.playground/Contents.swift
+++ TrapOfCollections.playground/Contents.swift
@@ -24,15 +24,34 @@ func isMoreThan(_ i: Int) -> (Int) -> Bool {
         return $0 > i
     }
 }
-var list = 1...10
-let newList = list.filter(isOdd())
-    .map(divide(2))
-    .map(multiply(5))
-    .filter(isMoreThan(10))
+func test(_ max: Int) -> (TimeInterval, TimeInterval) {
+    var start = Date()
+    let list = 1...max
+    let result1 = list.filter(isOdd())
+        .map(divide(2))
+        .map(multiply(5))
+        .filter(isMoreThan(10))
+        .prefix(5)
+    let time1 = Date().timeIntervalSince(start)
+    print(result1)
+    print()
+    start = Date()
+    let result2 = list.lazy
+        .filter(isOdd())
+        .map(divide(2))
+        .map(multiply(5))
+        .filter(isMoreThan(10))
+        .prefix(5)
+        .reduce(into: [Int] ()) { (array: inout [Int], i: Int) in array.append(i) }
+    let time2 = Date().timeIntervalSince(start)
+    print(result2)
+    print()
+    return (time1, time2)
+}
+
 print()
-let newList2 = list.lazy
-    .filter(isOdd())
-    .map(divide(2))
-    .map(multiply(5))
-    .filter(isMoreThan(10))
-    .reduce(into: [Int] ()) { (array: inout [Int], i: Int) in array.append(i) }
+let results = Array(1...2).map { _ in test(1000) }
+let ave1 = results.map { $0.0 }.reduce(0) { $0 + $1 } / Double(results.count)
+let ave2 = results.map { $0.1 }.reduce(0) { $0 + $1 } / Double(results.count)
+print("before: \(ave1)")
+print("after: \(ave2)")
